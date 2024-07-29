e933ea25b35962a78f20015f147477cdb8968e4d
saketh-24 <saketh1094@gmail.com>
2024-07-29 19:01:11 +0530

    added remove functionality

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { remove } from "../state/cartSlice";
 
 const Cart = () => {
-  return <div>cart</div>;
+  const products = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const handleRemove = (id) => {
+    dispatch(remove(id));
+  };
+
+  return (
+    <div className="max-w-fit mx-auto">
+      <h3 className="text-3xl p-4">Cart</h3>
+      <div className="cartContainer flex gap-11 flex-wrap items-center justify-center">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="cartCard p-10 flex flex-col justify-center items-center"
+          >
+            <img
+              className="w-[200px] h-[200px]"
+              src={product.image}
+              alt="image"
+            />
+            <h5>{product.title}</h5>
+            <h5>{product.price}</h5>
+            <button
+              onClick={() => handleRemove(product.id)}
+              className="w-[250px] text-white p-4 bg-blue-500"
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
--- src/state/cartSlice.jsx
+++ src/state/cartSlice.jsx
@@ -9,7 +9,7 @@ const cartSlice = createSlice({
     },
 
     remove(state, action) {
-      state.filter((item) => item.id !== action.payload);
+      return state.filter((item) => item.id !== action.payload);
     },
   }, // within reducers we write function to listen to actions
 });
